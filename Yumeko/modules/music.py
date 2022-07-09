2d310f45c379ab08aec2a7b2c4b0ed60eaf71390
nezukorobot <93088904+nezukorobot@users.noreply.github.com>
2022-07-09 07:52:49 +0530

    Update music.py

--- Yumeko/modules/music.py
+++ Yumeko/modules/music.py
@@ -119,51 +119,6 @@ async def download_song(url):
     return song
 
 
-# Jiosaavn Music
-
-
-@app.on_message(filters.command("saavn") & ~filters.edited)
-@capture_err
-async def jssong(_, message):
-    global is_downloading
-    if len(message.command) < 2:
-        return await message.reply_text(
-            "/saavn requires an argument."
-        )
-    if is_downloading:
-        return await message.reply_text(
-            "Another download is in progress, try again after sometime."
-        )
-    is_downloading = True
-    text = message.text.split(None, 1)[1]
-    m = await message.reply_text("Searching...")
-    try:
-        songs = await arq.saavn(text)
-        if not songs.ok:
-            await m.edit(songs.result)
-            is_downloading = False
-            return
-        sname = songs.result[0].song
-        slink = songs.result[0].media_url
-        ssingers = songs.result[0].singers
-        sduration = songs.result[0].duration
-        await m.edit("Downloading")
-        song = await download_song(slink)
-        await m.edit("Uploading")
-        await message.reply_audio(
-            audio=song,
-            title=sname,
-            performer=ssingers,
-            duration=sduration,
-        )
-        await m.delete()
-    except Exception as e:
-        is_downloading = False
-        return await m.edit(str(e))
-    is_downloading = False
-    song.close()
-
-
 # Lyrics
 
 
